458b241c069855d2c1f4f9dbbf68ef274dfa8e22
Jason Burton <53270059+jwburton@users.noreply.github.com>
2021-04-29 22:09:33 +0100

    updates with R feature

--- code/empirical_robustness.R
+++ code/empirical_robustness.R
@@ -1,69 +1,213 @@
-library(tidyverse) #for all data wrangling
-library(stargazer)
-library(lme4) #for lmer & glmer models
-library(sjmisc) 
-library(effects)
-library(sjstats) #use for r2 functions
-library(ggeffects)
+# clear environment
+rm(list = ls())
 
+# install + load packages
+pkgs <- c("tidyverse", "cowplot", "lme4", "sjPlot", 
+          "sjmisc", "effects", "sjstats", "ggeffects",
+          "stargazer", "e1071", "fitdistrplus", "evir")
 
+for (p in pkgs){
+  if(!require(p, character.only = TRUE)){
+    install.packages(p)
+    library(p, character.only = TRUE)
+  }
+}
+rm(p, pkgs)
 
-setwd("~/Dropbox (MIT)/projects/task_dependent_WOC/write-up/arxiv/data/")
+## Calculate skewness, kurtosis, and log-laplace rel. log lik. for each task ----
+# read in data from prior research 
+# assumes you've downloaded GitHub master folder to working directory
+df <- read.csv('./data/empirical_data/prior_research.csv')
 
+# create vector with task ids
+tasks <- unique(df$task_id) %>% as.character()
 
-######################## robustness_check
-data_robust.omega_01_n_5 <- read.csv('../data/robustness_data/prior_research_group_robust_omega_0.1_n_5.csv')
-data_robust.omega_033_n_50 <- read.csv('../data/robustness_data/prior_research_group_robust_omega_0.33_n_50.csv')
-data_robust.omega_1_n_35 <- read.csv('../data/robustness_data/prior_research_group_robust_omega_1.0_n_35.csv')
+## Skewness ----
 
-model.omega_01_n_5<- lmer(revised_abs_error_zscore ~ Omega_hat*influence + (1 | group_id), 
-                          data = data_robust.omega_01_n_5)
+# empty df for results
+skews <- data.frame(task_id = NA, skew = NA)
 
-model.omega_033_n_50<- lmer(revised_abs_error_zscore ~ Omega_hat*influence + (1 | group_id), 
-                            data = data_robust.omega_033_n_50)
+# loop through tasks and calc skewness with e1071::skewness
+for(i in c(1:54)){
+  # get estimates for a particular task
+  estimates <- df %>% 
+    filter(task_id == tasks[i]) %>%
+    filter(!is.na(pre_influence)) 
+  
+  # calculate skewness
+  s <- skewness(estimates$pre_influence)
+  
+  # create row with task id and skewness
+  task_skew <- c(paste0(tasks[i]), s)
+  
+  # bind to dataframe
+  skews <- as.data.frame(rbind(skews, task_skew))
+  
+  # remove row with NAs on first iteration
+  if(i==1){
+    skews <- filter(skews, !is.na(task_id))
+  }
+}
 
-model.omega_1_n_35<- lmer(revised_abs_error_zscore ~ Omega_hat*influence + (1 | group_id), 
-                          data = data_robust.omega_1_n_35)
+# tidy up environment
+rm(i, s, estimates, task_skew)
 
+# make skewness measures numeric
+skews$skew <- as.numeric(skews$skew)
 
-tab_model(model.omega_01_n_5,model.omega_033_n_50,model.omega_1_n_35,
-          collapse.ci = TRUE,
-          pred.labels = c("Intercept", "Omega", "Influence [0, 1]", "Omega x Influence"),
-          dv.labels = c("omega=0.1, n=5", "omega=1/3, n=50", "omega=1, n=35")
-)
+## Kurtosis ----
 
+# empty df for results
+kurts <- data.frame(task_id = NA, kurt = NA)
 
-stargazer(c(model.omega_01_n_5,model.omega_033_n_50,model.omega_1_n_35),
-          title="Robustness checks (by varying $omega$ and $n$ when calculating the task environment, $Omega$, from the empirical data) for the marginal effect of the interaction term between the centralization of influence and task environment on group performance---in terms of standardized absolute error. Each datapoint is an experimental trial. The results are from a mixed effect model with a random effect for the group ID.",
-          covariate.labels = c("Omega", "Influence [0, 1]", "Omega x Influence", "Intercept"),
-          column.labels =  c("omega=0.1, n=5", "omega=1/3, n=50", "omega=1, n=35"),
+# loop through tasks and calc skewness with e1071::kurtosis
+for(i in c(1:54)){
+  # get estimates for a particular task
+  estimates <- df %>% 
+    filter(task_id == tasks[i]) %>%
+    filter(!is.na(pre_influence)) 
+  
+  # calculate kurtosis
+  ku <- kurtosis(estimates$pre_influence)
+  
+  # create row with task id and kurtosis
+  task_kurt <- c(paste0(tasks[i]), ku)
+  
+  # bind to dataframe
+  kurts <- as.data.frame(rbind(kurts, task_kurt))
+  
+  # remove row with NAs on first iteration
+  if(i==1){
+    kurts <- filter(kurts, !is.na(task_id))
+  }
+}
+
+# tidy up environment
+rm(i, ku, estimates, task_kurt)
+
+# make skewness measures numeric
+kurts$kurt <- as.numeric(kurts$kurt)
+
+## Hill estimate of the tail index -----
+
+# empty df for results
+hill_ests <- data.frame(task_id = NA, hill_est = NA)
+
+# loop through tasks and calc hill estimate
+for(i in c(1:length(tasks))){
+  # get estimates for a particular task
+  estimates <- df %>% 
+    filter(task_id == tasks[i]) %>%
+    filter(!is.na(pre_influence)) 
+  
+  # Hill estimation
+  h <- hill(data = estimates$pre_influence, auto.scale = F)
+  
+  # in task 46, remove estimate == "Inf"
+  if(max(h$y)=="Inf"){
+    h$y <- h$y[-length(h$y)]
+  }
+  
+  # create row with task id and average alpha (in lieu of proper k selection)
+  task_hill <- c(paste0(tasks[i]), mean(h$y))
+  
+  # bind to dataframe
+  hill_ests <- as.data.frame(rbind(hill_ests, task_hill))
+  
+  # remove row with NAs on first iteration
+  if(i==1){
+    hill_ests <- filter(hill_ests, !is.na(task_id))
+  }
+}
+
+# tidy up environment
+rm(i, h, estimates, task_hill)
+
+# make hill estimates numeric
+hill_ests$hill_est <- as.numeric(hill_ests$hill_est)
+
+## read in + join data ----
+# read in
+data <- read.csv('./data/empirical_data/prior_research_group.csv')
+
+# join 
+data <- inner_join(data, kurts, by = "task_id")
+data <- inner_join(data, skews, by = "task_id")
+data <- inner_join(data, hill_ests, by = "task_id")
+
+## interaction models ----
+
+data$IV <- data$kurt
+model.kurtosis<- lmer(revised_abs_error_zscore ~ IV*influence + (1 | group_id), 
+                          data = data)
+tab_model(model.kurtosis)
+
+data$IV <- data$skew
+model.skewness<- lmer(revised_abs_error_zscore ~ IV*influence + (1 | group_id), 
+                            data = data)
+tab_model(model.skewness)
+
+data$IV <- data$hill_est
+model.hill<- lmer(revised_abs_error_zscore ~ IV*influence + (1 | group_id), 
+                          data = data)
+
+# inspect results
+tab_model(model.kurtosis,
+          model.skewness,
+          model.hill,
+          collapse.ci = F,
+          pred.labels = c("Intercept", "IV", "Influence [0, 1]", "DV x Influence"),
+          dv.labels = c("Kurtosis" 
+                        ,"Skewness" 
+                        ,"Hill Estimate"
+                        ))
+
+## print latex table S2 ----
+class(model.kurtosis) <- "lmerMod"
+class(model.skewness) <- "lmerMod"
+class(model.hill) <- "lmerMod"
+
+stargazer(c(model.kurtosis
+            ,model.skewness
+            ,model.hill
+            ),
+          title="Robustness checks (by varying the task environment metric, the independent variable (IV), derived from the empirical data) for the marginal effect of the interaction term between the centralization of influence and task environment on group performance---in terms of standardized absolute error. Each datapoint is an experimental trial. The results are from a mixed effect model with a random effect for the group ID.",
+          #covariate.labels = c("IV", "Social Influence [0, 1]", "IV x Social Influence", "Intercept"),
+          column.labels =  c("Kurtosis", "Skewness","Hill Estimate"
+          ),
           dep.var.labels = "Standardized Absolute Error"
           )
 
-plot_model(model.omega_01_n_5, type = "int")
-plot_model(model.omega_033_n_50, type = "int")
-plot_model(model.omega_1_n_50, type = "int")
+plot_model(model.kurtosis, type = "int")
+plot_model(model.skewness, type = "int")
+plot_model(model.hill, type = "int")
 
 
-########################
-data_robust.omega_01_n_5  <- data_robust.omega_01_n_5 [!(data_robust.omega_01_n_5 $influence==0),]
-data_robust.omega_033_n_50  <- data_robust.omega_033_n_50 [!(data_robust.omega_033_n_50 $influence==0),] #we can't say anything about solo as they can't improve
-data_robust.omega_1_n_35  <- data_robust.omega_1_n_35 [!(data_robust.omega_1_n_35 $influence==0),] #we can't say anything about solo as they can't improve
+## logistic models ----
+data <- data[!(data$influence==0),] #we can't say anything about solo as they can't improve
 
+data$IV <- data$kurt
+m.kurtosis <- glmer(improved ~ IV +  (1 | group_id),
+           data = data, family = binomial)
 
-m.omega_01_n_5 <- glmer(improved ~ Omega_hat +  (1 | group_id),
-           data = data_robust.omega_01_n_5, family = binomial)
+data$IV <- data$skew
+m.skewness <- glmer(improved ~ IV +  (1 | group_id),
+            data = data, family = binomial)
 
-m.omega_033_n_50 <- glmer(improved ~ Omega_hat +  (1 | group_id),
-            data = data_robust.omega_033_n_50, family = binomial)
+data$IV <- data$hill_est
+m.hill <- glmer(improved ~ IV +  (1 | group_id),
+            data = data, family = binomial)
 
-m.omega_1_n_35 <- glmer(improved ~ Omega_hat +  (1 | group_id),
-            data = data_robust.omega_1_n_35, family = binomial)
-
-stargazer(c(m.omega_01_n_5,m.omega_033_n_50,m.omega_1_n_35),
-          title="Robustness checks (by varying $omega$ and $n$ when calculating the task environment, $Omega$, from the empirical data) for the effect of the task environment on group performance after social interaction. Each datapoint is an experimental trial. The results are from a mixed effect model with a random effect for the group ID.",
-          covariate.labels = c("Omega", "Intercept"),
-          column.labels =  c("$omega=0.1, n=5$", "$omega=1/3, n=50$", "$omega=1, n=35$"),
+## print latex table S3 ----
+class(m.kurtosis) <- "lmerMod"
+class(m.skewness) <- "lmerMod"
+class(m.hill) <- "lmerMod"
+stargazer(c(m.kurtosis,m.skewness,m.hill),
+          title="Robustness checks (by varying the task environment metric, the independent variable IV, derived from the empirical data) for the effect of the task environment on group performance after social interaction. Each datapoint is an experimental trial. The results are from a mixed effect model with a random effect for the group.",
+          covariate.labels = c("IV", "Intercept"),
+          column.labels =  c("Kurtosis", "Skewness"
+                             , "Hill Estimate"
+                             ),
           dep.var.labels = "Whether the group improved after social interaction"
 )
 
--- code/regression.R
+++ code/regression.R
@@ -1,52 +1,189 @@
-library(tidyverse) #for all data wrangling
-library(cowplot) #for manuscript ready figures
-library(lme4) #for lmer & glmer models
-library(sjPlot) #for plotting lmer and glmer mods
-library(sjmisc) 
-library(effects)
-library(sjstats) #use for r2 functions
-library(ggeffects)
-library(ggplot2)
-library(stargazer)
+# predicting effect of (de)centralized influence on collective accuracy 
+# with relatiove log likelihoods: log-normal likelihood / (log-normal likelihood + normal likelihood) 
+# NOTE: to generate the final panel of plots, you should first run `descriptives_table.R`
 
+# clear environment -- except for desc_tab from `descriptives_table.R`
+rm(list=setdiff(ls(), "desc_tab"))
 
-setwd("~/Dropbox (MIT)/projects/task_dependent_WOC/write-up/arxiv/data/")
+# install + load packages
+pkgs <- c("tidyverse", "cowplot", "lme4", "sjPlot", 
+          "sjmisc", "effects", "sjstats", "ggeffects",
+          "stargazer", "e1071", "ggnewscale", "fitdistrplus")
 
-data <- read.csv('../data/empirical_data/prior_research_group.csv')
+for (p in pkgs){
+  if(!require(p, character.only = TRUE)){
+    install.packages(p)
+    library(p, character.only = TRUE)
+  }
+}
+rm(p, pkgs)
 
+# read in data from prior research 
+# assumes you've downloaded GitHub master folder to working directory
+df <- read.csv('./data/empirical_data/prior_research.csv')
+
+# create vector with task ids
+tasks <- unique(df$task_id) %>% droplevels() %>% as.character()
+
+# empty df for results
+RLLs <- data.frame(task_id = NA, rll = NA)
+
+# loop through tasks and calc rel log likelihoods
+for(i in c(1:length(tasks))){
+  # get estimates for a particular task
+  estimates <- df %>% 
+    filter(task_id == tasks[i]) %>%
+    filter(!is.na(pre_influence)) 
+  
+  # fit normal and log-normal dist to data
+  fit.norm <- fitdist(estimates$pre_influence+1, distr = "norm", method = "mle")
+  fit.lognorm <- fitdist(estimates$pre_influence+1, distr = "lnorm", method = "mle")
+  
+  # calculate rel log lik
+  relLL <- fit.norm$loglik/(fit.lognorm$loglik+fit.norm$loglik)
+
+  # create row with task id and tail weight
+  task_rll <- c(paste0(tasks[i]), relLL)
+
+  # bind to dataframe
+  RLLs <- as.data.frame(rbind(RLLs, task_rll))
+  
+  # remove row with NAs on first iteration
+  if(i==1){
+    RLLs <- filter(RLLs, !is.na(task_id))
+  }
+}
+
+# tidy up environment
+rm(i, estimates, task_rll, fit.lognorm, fit.norm)
+
+# make rel log lik numeric
+RLLs$rll <- as.numeric(RLLs$rll)
+
+# merging into old "regression.R" script
+# assumes you've downloaded GitHub master folder to working directory
+data <- read.csv('./data/empirical_data/prior_research_group.csv')
+
+# join rel log like data to `data`
+data <- inner_join(data, RLLs, by = "task_id")
+data$influence <- as.factor(data$influence)
 
 #### interaction on absolute error
-model.interaction <- lmer(revised_abs_error_zscore ~ Omega_hat*influence + (1 | group_id), 
-                     data = data)
+model.interaction <- lmer(revised_abs_error_zscore ~ rll*influence + (1 | group_id), 
+                          data = data)
 
 tab_model(model.interaction, show.stat=TRUE)
+plot_model(model.interaction, type = "int", show.data=T)
 
-plot_model(model.interaction, type = "int", show.data=TRUE)
+# make pretty plots
+RLLs$study <- with(RLLs, ifelse(str_detect(task_id, "lorenz2017"), "a_lorenz2011",
+                                ifelse(str_detect(task_id, "gurcay2015"), "b_gurcay2015",
+                                       ifelse(str_detect(task_id, "becker2017"), "c_becker2017", "d_becker2019")))) %>%
+  as.factor()
 
+my_cols <- c("a_lorenz2011"="firebrick",
+             "b_gurcay2015"="darkgoldenrod1", 
+             "c_becker2017"="cornflowerblue",
+             "d_becker2019"="azure4")
+
+p_dists <- ggplot(RLLs, aes(x = rll, color = study))+
+  geom_density(size = 0.9)+
+  geom_rug(alpha = 0.6, size = 1.5)+
+  theme_minimal()+
+  scale_color_manual(name = "Study", 
+                       values = my_cols,
+                       labels = c("c_becker2017"="Becker et al. 2017", "d_becker2019"="Becker et al. 2019", 
+                                  "b_gurcay2015"="Gürçay et al. 2015", "a_lorenz2011"="Lorenz et al. 2011"))+
+  labs(y = "Density")+
+  xlab(expression(italic("R")))+
+  theme(legend.position = "bottom")+
+  xlim(0,1)
+
+my_cols2 <- c("1"="chocolate1", "0"="chartreuse4")  
+pdat <- get_model_data(model.interaction, type = "int", show.data=T) %>% as.data.frame()
+p_int <- ggplot(pdat, aes(x=x, y=predicted, group=group, color=group))+
+  #stat_smooth(method="lm", aes(color = group), fullrange=TRUE, se=T)+
+  geom_smooth(method = "lm", show.legend = T)+ 
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high, fill = group, color = NULL), 
+              alpha = .1, show.legend = F)+
+  scale_color_manual(name = "", values = my_cols2, labels = c("0"="Decentralized","1"="Centralized"))+
+  scale_fill_manual(name = "", values = my_cols2, labels = c("0"="Decentralized","1"="Centralized"))+
+  new_scale_color()+
+  geom_rug(inherit.aes = F, data = RLLs, aes(x=rll, color=study), alpha=0.6, size = 1.5,
+           show.legend = F)+
+  scale_color_manual(name = "", 
+                     values = my_cols,
+                     labels = c("c_becker2017"="Becker et al. 2017", "d_becker2019"="Becker et al. 2019", 
+                                "b_gurcay2015"="Gürçay et al. 2015", "a_lorenz2011"="Lorenz et al. 2011"),
+                     guide = F)+  
+  labs(y = "Standardized Absolute Error")+
+  xlab(expression(italic("R")))+
+  annotate(x=0, y=2.2, geom="text",
+           label="N = 687", hjust = 0)+
+  annotate(x=0, y=2.05, geom="text",
+           label="t-statistic = -3.95", hjust = 0)+
+  annotate(x=0, y=1.9, geom="text",
+           label="italic(p) < 0.001", parse = T, hjust = 0)+
+  xlim(0,1)+
+  theme_minimal()+
+  theme(legend.position = c(0.24, 0.8))
 
 
 #probability of improvement
 
-data <- data[!(data$influence==0),] #we can't say anything about solo as they can't improve
+data <- data[!(data$influence==0),] # we can't say anything about solo as they can't improve
 
-m <- glmer(improved ~ Omega_hat +  (1 | group_id),
+m <- glmer(improved ~ rll +  (1 | group_id),
            data = data, family = binomial)
 
 summary(m)
 sjPlot::tab_model(m, show.re.var= TRUE, show.stat=TRUE)
 
 
-mydf <- ggpredict(m, "Omega_hat", add.data = TRUE)
-ggplot(mydf, aes(x, predicted)) +
-  geom_line() +  xlim(0, 1) + ylim(0, 1) +
-  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)
+mydf <- ggpredict(m, "rll [all]", add.data = TRUE)
+p_pred <- ggplot(mydf, aes(x, predicted)) +
+  geom_line() +
+  #stat_smooth(method="lm", fullrange=TRUE, se=T, color = "black")+
+  ylim(0, 1) +
+  xlim(0, 1) +
+  geom_ribbon(aes(ymin = conf.low, ymax = conf.high), alpha = .1)+
+  new_scale_color()+
+  geom_rug(inherit.aes = F, data = RLLs, aes(x=rll, color=study), alpha=0.6, size = 1.5,
+           show.legend = F)+
+  scale_color_manual(name = "", 
+                     values = my_cols,
+                     labels = c("c_becker2017"="Becker et al. 2017", "d_becker2019"="Becker et al. 2019", 
+                                "b_gurcay2015"="Gürçay et al. 2015", "a_lorenz2011"="Lorenz et al. 2011"),
+                     guide = F)+  
+  labs(y ="\u03a9")+
+  xlab(expression(italic("R")))+
+  annotate(x=0, y=1, geom="text",
+           label="N = 582", hjust = 0)+
+  annotate(x=0, y=0.955, geom="text",
+           label="z-statistic = 5.26", hjust = 0)+
+  annotate(x=0, y=0.905, geom="text",
+           label="italic(p) < 0.001", parse = T, hjust = 0)+
+  theme_minimal()
 
-
-stargazer(c(model.interaction,m), 
-          title="The main effects of task environment $Omega$ and the interaction with social influence (i.e., centralization) Each datapoint is an experimental trial. The results are from a mixed effect model with a random effect for the group ID.",
-          covariate.labels = c("$Omega$", "Influence", "$Omega$ x Influence", "Intercept"),
+class(model.interaction) <- "lmerMod"
+class(m) <- "lmerMod"
+stargazer(c(m, model.interaction), 
+          title="The main effects of task environment (relative log likelihood; R) and the interaction with social influence (i.e., centralization) Each datapoint is an experimental trial. The results are from a mixed effect model with a random effect for the group",
+          covariate.labels = c("R", "Influence", "R x Influence", "Intercept"),
           column.labels =  NULL,
-          dep.var.labels = c("Standardized Absolute Error", "Group improved after social interaction")
+          dep.var.labels = c("Group improved after social interaction", "Standardized Absolute Error")
 )
 
 
+# create + save panel
+t_row <- cowplot::plot_grid(desc_tab, 
+                            p_dists+theme(legend.position = "none"), 
+                            ncol = 1, labels = c("A", "B"), rel_heights = c(0.6, 1))
+b_row <- cowplot::plot_grid(p_pred, 
+                            p_int, 
+                            ncol = 2, labels = c("C", "D"))
+col_leg <- cowplot::get_legend(p_dists+theme(legend.position = "bottom"))
+
+#png("fig3.png", width = 6.5, height = 9.1, units = "in", res = 500)
+cowplot::plot_grid(t_row, col_leg, b_row, ncol = 1, rel_heights = c(1, 0.1, 1))
+#dev.off()
